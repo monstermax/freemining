43786ea80290fc160015dab040d05c3eda00f339
Max YO <max@monstermax.net>
2023-01-30 06:17:39 +0100

    dev 2023-01

--- dist/core/http/routesCore.js
+++ dist/core/http/routesCore.js
@@ -2,14 +2,15 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.registerCoreRoutes = void 0;
 const tslib_1 = require("tslib");
+const path_1 = tslib_1.__importDefault(require("path"));
 const utils_1 = require("../../common/utils");
 function registerCoreRoutes(app, urlPrefix = '') {
-    app.get(urlPrefix + '/', (req, res, next) => tslib_1.__awaiter(this, void 0, void 0, function* () {
+    app.get(urlPrefix + path_1.default.sep, (req, res, next) => tslib_1.__awaiter(this, void 0, void 0, function* () {
         const data = {
             now: utils_1.now,
-            contentTemplate: './homepage.html',
+            contentTemplate: `.${path_1.default.sep}homepage.html`,
         };
-        res.render('core/layout.html', data);
+        res.render(`core${path_1.default.sep}layout.html`, data);
     }));
 }
 exports.registerCoreRoutes = registerCoreRoutes;
