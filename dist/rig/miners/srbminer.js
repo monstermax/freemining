7a83ec0a0a52d88a896a443cc6daddc04f004c71
Max YO <max@monstermax.net>
2023-09-13 08:05:00 +0200

    2023-09

--- dist/rig/miners/srbminer.js
+++ dist/rig/miners/srbminer.js
@@ -20,7 +20,8 @@ Download : https://github.com/doktor83/SRBMiner-Multi/releases/
 const minerName = 'srbminer';
 const minerTitle = 'SRBMiner Multi';
 const github = 'doktor83/SRBMiner-Multi';
-const lastVersion = '2.2.3';
+//const lastVersion = '2.2.3';
+const lastVersion = '2.3.5';
 /* ########## MAIN ######### */
 const SEP = path_1.default.sep;
 /* ########## FUNCTIONS ######### */
